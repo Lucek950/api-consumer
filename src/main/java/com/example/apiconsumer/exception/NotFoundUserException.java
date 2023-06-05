81b1062b61f660eea23041048057ef86940baa01
Michał Ulik <michal.ulik@ppuc.pl>
2023-06-05 16:34:27 +0200

    Create github error message handler

--- src/main/java/com/example/apiconsumer/exception/NotFoundUserException.java
+++
@@ -1,8 +0,0 @@
-package com.example.apiconsumer.exception;
-
-public class NotFoundUserException extends RuntimeException {
-
-  public NotFoundUserException(String username) {
-    super(String.format("Not found user: %s", username));
-  }
-}
